8f67ebb0a136408458ef4db790b6856f420613d7
fo-ji <fumiya1753@gmail.com>
2023-05-10 17:38:07 +0900

    fix

--- src/components/TodoList.test.tsx
+++ src/components/TodoList.test.tsx
@@ -1,50 +1,50 @@
-import { screen, waitFor } from '@testing-library/react';
-import { expect, test, vi } from 'vitest';
+import { screen, waitFor } from "@testing-library/react";
+import { expect, test, vi } from "vitest";
 
-import { customRender } from '@/testing';
+import { customRender } from "@/testing";
 
-import { TodoList } from './TodoList';
+import { TodoList } from "./TodoList";
 
-test('render loading text correctly', () => {
+test("render loading text correctly", () => {
   customRender(<TodoList />);
-  expect(screen.getByText('Loading...')).toBeInTheDocument();
+  expect(screen.getByText("Loading...")).toBeInTheDocument();
 });
 
-test('render TodoList correctly', async () => {
+test("render TodoList correctly", async () => {
   customRender(<TodoList />);
-  expect(await screen.findByText('TodoList')).toBeInTheDocument();
+  expect(await screen.findByText("TodoList")).toBeInTheDocument();
 });
 
-test('add todo item correctly', async () => {
+test("add todo item correctly", async () => {
   const { user } = customRender(<TodoList />);
-  const textbox = await screen.findByRole('textbox');
-  await user.type(textbox, 'testAddTodo');
-  await user.click(screen.getByRole('button', { name: 'add' }));
-  expect(await screen.findByText('👀 testAddTodo')).toBeInTheDocument();
+  const textbox = await screen.findByRole("textbox");
+  await user.type(textbox, "testAddTodo");
+  await user.click(screen.getByRole("button", { name: "add" }));
+  expect(await screen.findByText("👀 testAddTodo")).toBeInTheDocument();
 });
 
-test('update todo item correctly', async () => {
+test("update todo item correctly", async () => {
   const { user } = customRender(<TodoList />);
-  await user.click((await screen.findAllByRole('checkbox'))[0]);
+  await user.click((await screen.findAllByRole("checkbox"))[0]);
   await waitFor(
-    () => void expect(screen.getAllByRole('checkbox')[0]).toBeChecked()
+    () => void expect(screen.getAllByRole("checkbox")[0]).toBeChecked()
   );
 });
 
-test('delete todo item correctly', async () => {
+test("delete todo item correctly", async () => {
   const { user } = customRender(<TodoList />);
-  const windowConfirmSpy = vi.spyOn(window, 'confirm');
+  const windowConfirmSpy = vi.spyOn(window, "confirm");
   windowConfirmSpy.mockImplementation(() => true);
   await waitFor(
-    () => void expect(screen.getAllByRole('listitem').length).toBe(2)
+    () => void expect(screen.getAllByRole("listitem").length).toBe(2)
   );
   await user.click(
     (
-      await screen.findAllByRole('button', { name: 'delete' })
+      await screen.findAllByRole("button", { name: "delete" })
     )[0]
   );
   await waitFor(
-    () => void expect(screen.getAllByRole('listitem').length).toBe(1)
+    () => void expect(screen.getAllByRole("listitem").length).toBe(1)
   );
   windowConfirmSpy.mockRestore();
 });
